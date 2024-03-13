975a0780386a190d57999d70e80a030a2416a33e
Caco0 <cacotpds@gmail.com>
2024-03-13 09:49:02 -0300

    Added code to select candidates based on their salary expectations.

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,37 @@
 package candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.00);
-        analisarCandidato(2200.00);
-        analisarCandidato(2000.00);
-
-
+        selecaoCnadidatos();
     }
+
+    static void selecaoCnadidatos(){
+       String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+
+       int candidatosSelecionados = 0;
+       int candidatoAtual = 0;
+       double salarioBase = 2000.00;
+
+       while (candidatosSelecionados <  5 && candidatoAtual < candidatos.length) {
+        String candidato = candidatos[candidatoAtual];
+        double salarioPretendido = valorPretendido();
+        System.out.println("O candidato" + candidato + " Solicitou este salário" + salarioPretendido);
+
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("O candidato "+candidato+ " foi selecionado para a vaga!");
+            candidatosSelecionados++;
+        }
+        candidatoAtual++;
+       }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+    
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.00; //Salário Base do Cargo
         if (salarioBase > salarioPretendido){
